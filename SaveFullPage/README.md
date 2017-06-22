bfb2ec62e83e6a0992ddc7d565132812fcfc1bb7
Stekolschikov <stekolschikov@mail.ua>
2017-06-22 14:55:27 +0300

    mornong 22/06

--- SaveFullPage/README.md
+++ SaveFullPage/README.md
@@ -3,3 +3,20 @@ http://5.tutcravt1.tutdomen.com/
 npm install
 
 electron-packager . myapp --ignore="node_modules/(electron-packager|electron-p rebuilt)" --platform=win32 --arch=ia32 --version=0.36.3 --overwrite --asar=false
+
+{
+  "URL": "",
+  "NAME":"",
+  "HOST": "",
+  "PORT": "21",
+  "USERNAME": "",
+  "PASSWORD": "",
+  "dirOnServer": "",
+  "dirOnPC": "",
+  "HOST_TOW": "",
+  "PORT_TOW": "",
+  "USERNAME_TOW": "",
+  "PASSWORD_TOW": "",
+  "dirOnServer_TOW": "",
+  "dirOnPC_TOW": ""
+}
--- SaveFullPage/render.js
+++ SaveFullPage/render.js
@@ -20,10 +20,13 @@ var ftpDeploy = new FtpDeploy();
 var exec = require('child_process').exec;
 var cmd=require('node-cmd');
 var jsonfile = require('jsonfile');
+
+
+// --- --- --- conf and data
+var confAndData = {}
 // conf and data --- --- ---
-var confAndData = { }
-// conf and data --- --- ---
-// appInfo --- --- ---
+
+// --- --- --- appInfo
 var appInfo = {
     // VARS
     fileName: 'data.json',
@@ -49,8 +52,8 @@ var appInfo = {
     // METHODS
     get: function () {
         url = $('#URL').val(); name = $('#NAME').val();
-        dir = path.join(__dirname + '../../../', 'save', name);
-        // dir = path.join(__dirname, 'save', name);
+        // dir = path.join(__dirname + '../../../', 'save', name);
+        dir = path.join(__dirname, 'save', name);
     }, say: function (text) {
         $.notify(text, { allow_dismiss: false });
         var audio = new Audio(); audio.src = 'viber.mp3'; audio.autoplay = true;
@@ -98,132 +101,139 @@ var appInfo = {
     }
 };
 // appInfo --- --- ---
+
+// --- --- --- appPars
+var appPars = {
+
+    // LINE
+    line: function () {
+        $( ".loader-block" ).fadeIn( "slow" );
+        appInfo.get();
+        if (fs.existsSync(dir)) {
+            appPars.removeLocalDir(dir);
+        }
+        else {
+            var tmpVideoUrl = '';
+            if ($('#video').is(":checked")) { options = { urls: [url], directory: dir }; }
+            else {
+                options = {
+                    urls: [url], directory: dir, sources: [
+                        {selector: 'img', attr: 'src'}, {selector: 'link[rel="stylesheet"]', attr: 'href'},
+                        {selector: 'script', attr: 'src'}, {selector: 'video', attr: 'src'} ],
+                    onResourceSaved: (resource) => {
+                        if(resource.url.indexOf('http://play999.ru/index.php') + 1) { tmpVideoUrl = resource; }
+                    }
+                };
+            }
+            // get webpage
+            scrape(options).then((result) => {
+                appInfo.say('<strong>ПАРСИНГ УСПЕШЕН!</strong> проверьте папку ' + dir);
+                appInfo.fade("fadeOut");
+                if(tmpVideoUrl != '')
+                {
+                    fs.readFile(path.join(dir, 'index.html'), 'utf8', function (err,data) {
+                        var str = data.replace(tmpVideoUrl.filename,tmpVideoUrl.url);
+                        fs.writeFile(path.join(dir, 'index.html'), str, function(err) {});
+                    });
+                }
+            }).catch((err) => { appInfo.say('<strong>ОШИБКА!</strong> ' + err); appInfo.fade("fadeOut"); });
+        }
+    },
+    // EVENTS
+    events: function () { $('#GET').click(function () { appPars.line() }); },
+    // METHODS
+    removeLocalDir: function (src) {
+        fsextra.remove(src, err => {
+            if (err) appInfo.say('<strong>УДАЛЕНИЕ СТАРОЙ ПАПКИ!</strong> неуспешно!');
+            else appInfo.say('<strong>УДАЛЕНИЕ СТАРОЙ ПАПКИ!</strong> успешно!');
+            appPars.line();
+        })
+    }
+};
+// appPars --- --- ---
+
+// --- --- --- appFtp
+appFtp = {
+    // LINE
+    line: function () {
+        appFtp.get();
+        $( ".loader-block" ).fadeIn( "slow" );
+        try {
+            var Ftp = new JSFtp({ host: HOST, port: PORT, user: USERNAME, pass: PASSWORD });
+            Ftp.ls(".", function(err, res) {
+                var suchFolderIs = false;
+                res.forEach(function(file) { if(file.name == name) { suchFolderIs = true; } });
+                if (suchFolderIs == false) mkd();
+                else upload();
+            });
+            function mkd() {
+                Ftp.raw("mkd", "/" + dirOnServer, function(err, data) {
+                    if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+                    else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); upload(); } });
+            }
+            function upload() {
+                var config = { username: Ftp.user, password: Ftp.pass, host: Ftp.host,  port: Ftp.port,
+                    localRoot: dir, remoteRoot: "/" + dirOnServer };
+                ftpDeploy.deploy(config, function(err) {
+                    if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+                    else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); appInfo.fade('fadeOut'); }
+                });
+            }
+
+        } catch (err){ appInfo.say('<strong>ОШИБКА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+    },
+    // EVENTES
+    events: function () { $('#FTPUPLOAD').click(function () { appFtp.line(); }); },
+    // METHODS
+    get: function () {
+        HOST = $('#HOST').val(); PORT = $('#PORT').val(); USERNAME = $('#USERNAME').val();
+        dirOnServer = $('#dirOnServer').val(); dirOnPC = $('#dirOnPC').val(); PASSWORD = $('#PASSWORD').val();
+    }
+};
+// appFtp --- --- ---
+
+// --- --- --- appFtpTwo
+appFtpTwo = {
+    // LINE
+    line: function () {
+        appFtpTwo.get();
+        $( ".loader-block" ).fadeIn( "slow" );
+        try {
+            var Ftp = new JSFtp({ host: HOST, port: PORT, user: USERNAME, pass: PASSWORD });
+            Ftp.ls(".", function(err, res) {
+                var suchFolderIs = false;
+                res.forEach(function(file) { if(file.name == name) { suchFolderIs = true; } });
+                if (suchFolderIs == false) mkd();
+                else upload();
+            });
+            function mkd() {
+                Ftp.raw("mkd", "/" + dirOnServer, function(err, data) {
+                    if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+                    else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); upload(); } });
+            }
+            function upload() {
+                var config = { username: Ftp.user, password: Ftp.pass, host: Ftp.host,  port: Ftp.port,
+                    localRoot: dir, remoteRoot: "/" + dirOnServer };
+                ftpDeploy.deploy(config, function(err) {
+                    if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+                    else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); appInfo.fade('fadeOut'); }
+                });
+            }
+
+        } catch (err){ appInfo.say('<strong>ОШИБКА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
+    },
+    // EVENTES
+    events: function () { $('#FTPUPLOAD_TOW').click(function () { appFtpTwo.line(); }); },
+    // METHODS
+    get: function () {
+        HOST = $('#HOST_TOW').val(); PORT = $('#PORT_TOW').val(); USERNAME = $('#USERNAME_TOW').val();
+        dirOnServer = $('#dirOnServer_TOW').val(); dirOnPC = $('#dirOnPC_TOW').val(); PASSWORD = $('#PASSWORD_TOW').val();
+    }
+};
+// appFtpTwo --- --- ---
+
 $(document).ready(function () {
     $( ".loader-block" ).fadeOut( "slow" );
-
-    var appPars = {
-
-        // LINE
-        line: function () {
-            $( ".loader-block" ).fadeIn( "slow" );
-            appInfo.get();
-            if (fs.existsSync(dir)) {
-                appPars.removeLocalDir(dir);
-            }
-            else {
-                var tmpVideoUrl = '';
-                if ($('#video').is(":checked")) { options = { urls: [url], directory: dir }; }
-                else {
-                    options = {
-                        urls: [url], directory: dir, sources: [
-                            {selector: 'img', attr: 'src'}, {selector: 'link[rel="stylesheet"]', attr: 'href'},
-                            {selector: 'script', attr: 'src'}, {selector: 'video', attr: 'src'} ],
-                        onResourceSaved: (resource) => {
-                            if(resource.url.indexOf('http://play999.ru/index.php') + 1) { tmpVideoUrl = resource; }
-                        }
-                    };
-                }
-                // get webpage
-                scrape(options).then((result) => {
-                    appInfo.say('<strong>ПАРСИНГ УСПЕШЕН!</strong> проверьте папку ' + dir);
-                    appInfo.fade("fadeOut");
-                    if(tmpVideoUrl != '')
-                    {
-                        fs.readFile(path.join(dir, 'index.html'), 'utf8', function (err,data) {
-                            var str = data.replace(tmpVideoUrl.filename,tmpVideoUrl.url);
-                            fs.writeFile(path.join(dir, 'index.html'), str, function(err) {});
-                        });
-                    }
-                }).catch((err) => { appInfo.say('<strong>ОШИБКА!</strong> ' + err); appInfo.fade("fadeOut"); });
-            }
-        },
-        // EVENTS
-        events: function () { $('#GET').click(function () { appPars.line() }); },
-        // METHODS
-        removeLocalDir: function (src) {
-            fsextra.remove(src, err => {
-                if (err) appInfo.say('<strong>УДАЛЕНИЕ СТАРОЙ ПАПКИ!</strong> неуспешно!');
-                else appInfo.say('<strong>УДАЛЕНИЕ СТАРОЙ ПАПКИ!</strong> успешно!');
-                appPars.line();
-            })
-        }
-    };
-
-    appFtp = {
-        // LINE
-        line: function () {
-            appFtp.get();
-            $( ".loader-block" ).fadeIn( "slow" );
-            try {
-                var Ftp = new JSFtp({ host: HOST, port: PORT, user: USERNAME, pass: PASSWORD });
-                Ftp.ls(".", function(err, res) {
-                    var suchFolderIs = false;
-                    res.forEach(function(file) { if(file.name == name) { suchFolderIs = true; } });
-                    if (suchFolderIs == false) mkd();
-                    else upload();
-                });
-                function mkd() {
-                    Ftp.raw("mkd", "/" + dirOnServer, function(err, data) {
-                        if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-                        else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); upload(); } });
-                }
-                function upload() {
-                    var config = { username: Ftp.user, password: Ftp.pass, host: Ftp.host,  port: Ftp.port,
-                        localRoot: dir, remoteRoot: "/" + dirOnServer };
-                    ftpDeploy.deploy(config, function(err) {
-                        if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-                        else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); appInfo.fade('fadeOut'); }
-                    });
-                }
-
-            } catch (err){ appInfo.say('<strong>ОШИБКА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-        },
-        // EVENTES
-        events: function () { $('#FTPUPLOAD').click(function () { appFtp.line(); }); },
-        // METHODS
-        get: function () {
-            HOST = $('#HOST').val(); PORT = $('#PORT').val(); USERNAME = $('#USERNAME').val();
-            dirOnServer = $('#dirOnServer').val(); dirOnPC = $('#dirOnPC').val(); PASSWORD = $('#PASSWORD').val();
-        }
-    };
-
-    appFtpTwo = {
-        // LINE
-        line: function () {
-            appFtpTwo.get();
-            $( ".loader-block" ).fadeIn( "slow" );
-            try {
-                var Ftp = new JSFtp({ host: HOST, port: PORT, user: USERNAME, pass: PASSWORD });
-                Ftp.ls(".", function(err, res) {
-                    var suchFolderIs = false;
-                    res.forEach(function(file) { if(file.name == name) { suchFolderIs = true; } });
-                    if (suchFolderIs == false) mkd();
-                    else upload();
-                });
-                function mkd() {
-                    Ftp.raw("mkd", "/" + dirOnServer, function(err, data) {
-                        if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-                        else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); upload(); } });
-                }
-                function upload() {
-                    var config = { username: Ftp.user, password: Ftp.pass, host: Ftp.host,  port: Ftp.port,
-                        localRoot: dir, remoteRoot: "/" + dirOnServer };
-                    ftpDeploy.deploy(config, function(err) {
-                        if (err) { appInfo.say('<strong>ОШИБКА СОЗДНАИЯ ПАПКА НА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-                        else { appInfo.say('<strong>СОЗДАНА НОВАЯ ПАПКА НА FTP!</strong>'); appInfo.fade('fadeOut'); }
-                    });
-                }
-
-            } catch (err){ appInfo.say('<strong>ОШИБКА FTP!</strong> ' + err); appInfo.fade('fadeOut'); }
-        },
-        // EVENTES
-        events: function () { $('#FTPUPLOAD_TOW').click(function () { appFtpTwo.line(); }); },
-        // METHODS
-        get: function () {
-            HOST = $('#HOST_TOW').val(); PORT = $('#PORT_TOW').val(); USERNAME = $('#USERNAME_TOW').val();
-            dirOnServer = $('#dirOnServer_TOW').val(); dirOnPC = $('#dirOnPC_TOW').val(); PASSWORD = $('#PASSWORD_TOW').val();
-        }
-    };
     appInfo.line();
     appInfo.events();
     appPars.events();
